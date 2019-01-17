9abc3764556c926e67bfcbc2060a403ba73e9c93
Verevkina T <verevkina@picom.ru>
2019-01-17 18:15:15 +0400

    header

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,64 @@
 <template>
-  <v-layout>
-    <v-flex xs12 sm6 offset-sm3>
-      <v-card>
-        <v-img
-          src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
-          aspect-ratio="2.75"
-        ></v-img>
+    <v-app>
+      <v-navigation-drawer
+        app
+        temporary
+        v-model="drawer"
+        >
+        <v-list>
+            <v-list-tile
+              v-for="link of links"
+              :key="link.title"
+              :to="link.url"
+            >
+              <v-list-tile-action>
+                <v-icon>{{link.icon}}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title v-text="link.title"></v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
 
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-            <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
-          </div>
-        </v-card-title>
+      <v-toolbar app dark color="primary">
+        <v-toolbar-side-icon
+          @click="drawer = !drawer"
+          class="hidden-md-and-up"
+        ></v-toolbar-side-icon>
+        <v-toolbar-title>Sport application</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn
+            v-for="link in links"
+            :key="link.title"
+            :to="link.url"
+            flat>
+            <v-icon left>{{link.icon}}</v-icon>
+            {{link.title}}</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
 
-        <v-card-actions>
-          <v-btn flat color="orange">Share</v-btn>
-          <v-btn flat color="orange">Explore</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+      <v-content>
+        <router-view></router-view>
+      </v-content>
+  </v-app>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //
   },
   data () {
     return {
-      //
+      drawer: false,
+      links: [
+        {title: 'Description', icon: 'info', url: '/description'}
+      ]
     }
   }
 }
